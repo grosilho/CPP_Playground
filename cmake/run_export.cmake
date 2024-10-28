e0b633e3dcfd30981bed7c00a9c335f94df864e7
Giacomo <jackrosilho@gmail.com>
2024-10-28 14:58:35 +0100

    added presets for tests

--- cmake/run_export.cmake
+++ cmake/run_export.cmake
@@ -3,9 +3,9 @@
 # So far, in subdirectories, we packaged all the target information to be exported, but we still need to create a *.cmake files that will be used by other projects to find and use this library with find_package()
 # here we export the target information to a file called ${PROJECT_NAME}Targets.cmake
 install(EXPORT "${PROJECT_NAME}Targets"
-        FILE "${PROJECT_NAME}Targets.cmake"
-        NAMESPACE "${PROJECT_NAME}::" # add a namespace to the targets
         DESTINATION "lib/cmake/${PROJECT_NAME}"
+        NAMESPACE "${PROJECT_NAME}::" # add a namespace to the targets
+        FILE "${PROJECT_NAME}Targets.cmake"
 )
 include(CMakePackageConfigHelpers)
 # create the main config file. It will take the content of the Config.cmake.in file and replace the @PACKAGE_INIT@ with relative paths. The result will be saved in ${PROJECT_NAME}Config.cmake file.
@@ -30,4 +30,5 @@ install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Config.cmake"
 # To use it from a build directory we need to generate ${PROJECT_NAME}Targets.cmake in the build directory so that it can be used by the ${PROJECT_NAME}Config.cmake file there.
 export(EXPORT "${PROJECT_NAME}Targets"
         FILE "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}Targets.cmake"
+        NAMESPACE "${PROJECT_NAME}::"
         )
